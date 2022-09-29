347f6a31ff4ee85be8c3d91ac65f3df9dc269a4f
Ecsedi Milan <milan200314@gmail.com>
2022-09-29 15:31:54 +0200

    Comit init -dark mode kész -inputok elhelyezve -kép elhelyezve

--- src/index.js
+++ src/index.js
@@ -1,2 +1,53 @@
-import './style.css';
 
+
+
+function dark(){
+
+   document.getElementById('body').classList.add('DarkMode');
+   
+
+}
+function kepszeles(){
+
+       // let url= document.getElementById('inputkep').value;
+        let kepszel= document.getElementById('inputkepszelesseg').value;
+        
+        let keretsz= document.getElementById('inputkeretszin').value;
+
+        
+
+    return  document.getElementById('akep').width = kepszel;
+   
+
+}
+function kepkeret(){
+
+    let keretvast= document.getElementById('inputkeretvastagsag').value;
+
+
+    return document.getElementById('akep').style.borderRadius= keretvast;
+}
+ 
+function kepkszin(){
+
+
+    let keretszin= document.getElementById('inputkeretszin').value;
+
+return document.getElementById('akep').style.borderColor = keretszin;
+
+}
+
+
+ function init(){
+
+
+        document.getElementById('darkmode').addEventListener('click', dark);
+        document.getElementById('inputkep').addEventListener('click', kepeditor);
+        document.getElementById('inputkeretvastagsag').addEventListener('change', kepkeret);
+        document.getElementById('inputkepszelesseg').addEventListener('change', kepszeles);
+        document.getElementById('inputkeretszin').addEventListener('change', kepkszin);
+
+
+}
+
+document.addEventListener("DOMContentLoaded", init);
